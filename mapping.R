3a18664fc6371bd3bc41b0228e1f5aeed207d2b7
k8hertweck <k8hertweck@gmail.com>
2016-06-30 14:15:54 -0500

    adding shapefile creation and new mapping

--- mapping.R
+++ mapping.R
@@ -3,6 +3,8 @@
 ## load libraries
 library(fields)
 library(dplyr)
+library(dismo)
+library(maptools)
 
 ## load data with taxa in different files
 diploid <- read.csv(file="taxaData/taxa1.csv")
@@ -19,3 +21,29 @@ tetraploid <- both %>%
 US(xlim=c(-85,-77), ylim=c(26,37))
 points(diploid$lon, diploid$lat, col='orange', pch=20)
 points(tetraploid$lon, tetraploid$lat, col='red', pch=20)
+
+## a slightly more refined map (using built-in state outlines)
+southeast <- c("florida", "georgia", "north carolina", "south carolina")
+map(database="state", regions = southeast, interior=T, lwd=2)
+points(diploid$lon, diploid$lat, col='orange', pch=20, cex=2)
+points(tetraploid$lon, tetraploid$lat, col='red', pch=20, cex=2)
+
+## using US census shapefiles, save custom shapefile for modeling later
+# download, unzip all state shapefiles to new directory
+download.file("http://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_state_20m.zip", "cb_2015_us_state_20m.zip")
+dir.create("shapefiles")
+unzip("cb_2015_us_state_20m.zip", exdir="shapefiles")
+# load shapefiles and set projection
+state <- readShapePoly("shapefiles/cb_2015_us_state_20m.shp") 
+projection(state) <- CRS("+proj=longlat +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +no_defs")
+# extract shapefiles of interest and save to file 
+southeastStatesCap <- c("Florida", "Georgia", "North Carolina", "South Carolina")
+SEstates <- state[as.character(state@data$NAME) %in% southeastStatesCap, ]
+writeSpatialShape(SEstates, "shapefiles/SEstates")
+# map using custom shapefile and save to file
+dir.create("figures")
+pdf(file="figures/mapping.pdf")
+map(SEstates)
+points(diploid$lon, diploid$lat, col='orange', pch=20, cex=2)
+points(tetraploid$lon, tetraploid$lat, col='red', pch=20, cex=2)
+dev.off()
