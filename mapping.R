63292306b8b4c1aadb1d00cfbf00fa8110f812ff
k8hertweck <k8hertweck@gmail.com>
2016-06-30 13:55:57 -0500

    clarifying methods of data entry

--- mapping.R
+++ mapping.R
@@ -2,10 +2,18 @@
 
 ## load libraries
 library(fields)
+library(dplyr)
 
-## load data
-sp1 <- read.table(file="speciesData/species1.csv",  header=TRUE,  sep=",")
-sp2 <- read.table(file="speciesData/species2.csv",  header=TRUE,  sep=",")
+## load data with taxa in different files
+diploid <- read.csv(file="taxaData/taxa1.csv")
+tetraploid <- read.csv(file="taxaData/taxa2.csv")
+
+## load data with taxa in same files (does same thing as above, in different way)
+both <- read.csv(file="taxaData/bothTaxa.csv")
+diploid <- both %>%
+  filter(cytotype=="2X")
+tetraploid <- both %>%
+  filter(cytotype=="4X")
 
 ## quick and dirty plot on map (could also plot points first and add map)
 US(xlim=c(-85,-77), ylim=c(26,37))
