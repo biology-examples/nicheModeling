429ae0c19335bc692c48911d07e5a750b56de2b5
k8hertweck <k8hertweck@gmail.com>
2016-06-30 17:28:46 -0500

    initial commit with basic options

---
+++ maxent.R
@@ -0,0 +1,42 @@
+## running maxent in R
+# use ?maxent to check documentation for correct installation of maxent.jar
+
+# load libraries
+library(dismo)
+library(fields)
+library(maps)
+library(rgdal)
+library(raster)
+library(maptools)
+library(dplyr)
+library(rJava)
+
+# import occurrence data and convert to format required by maxent
+both <- read.csv(file="taxaData/bothTaxa.csv")
+diploid <- both %>%
+  filter(cytotype=="2X")
+diploid <- diploid[,c(3,2)]
+tetraploid <- both %>%
+  filter(cytotype=="4X")
+tetraploid <- tetraploid[,c(3,2)]
+
+# create stack of non-correlated layers (as determined by layerPrep.R)
+predictors <- stack(alt, bio2, bio3, bio5, bio6, bio8, bio9, bio12, bio13, bio14, bio19) 
+plot(predictors)
+
+# run maxent for diploid
+maxDip <- maxent(predictors, diploid)
+maxDip # views results in browser window
+response(maxDip) # show response curves for each layer
+rDip <- predict(maxDip, predictors) # create model
+plot(rDip)
+points(diploid)
+
+# run maxent for tetraploid
+maxTetra <- maxent(predictors, tetraploid) # run with default parameters
+maxTetra <- maxent(predictors, tetraploid, args=c("randomseed=true", "replicatetype=crossvalidate", "replicates=640", "-J")) # run with more parameters, takes much longer!
+maxTetra # views results in browser window
+response(maxTetra) # show response curves for each layer
+rTetra <- predict(maxTetra, predictors) # create model 
+plot(rTetra)
+points(tetraploid)
