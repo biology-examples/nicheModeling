efd6f55508acb2543742f2f596030896ea850cca
k8hertweck <k8hertweck@gmail.com>
2016-07-07 15:07:36 -0500

    outlining niche and non-niche based methods

--- nicheOverlap.R
+++ nicheOverlap.R
@@ -1,8 +1,11 @@
 ## evaluating niche overlap between taxa
 
-library(dismo)
-library(fields)
-library(maps)
-library(rgdal)
-library(raster)
-library(maptools)
+library(ecospat)
+library(enmtools)
+
+## multi-variate climate space comparisons (non-model based overlap assessment)
+# one-way ANOVA with Tukey's post-hoc
+
+# PCA with varimax rotation and Kaiser criterion (eigenvalues greater than or equal to 1) when choosing factors to include in PCA
+
+## assessing niche overlap
