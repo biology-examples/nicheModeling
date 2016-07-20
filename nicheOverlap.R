f2264f6b7ce481fd5905ec8d73a5c68915e56b00
k8hertweck <k8hertweck@gmail.com>
2016-07-20 18:46:14 -0500

    model import and nicheOverlap

--- nicheOverlap.R
+++ nicheOverlap.R
@@ -2,6 +2,12 @@
 
 library(ecospat)
 library(enmtools)
+library(raster)
+library(dismo)
+
+# read in models (raster)
+rDip <- raster("models/diploid.grd")
+rTetra <- raster("models/tetraploid.grd")
 
 ## multi-variate climate space comparisons (non-model based overlap assessment)
 # one-way ANOVA with Tukey's post-hoc
@@ -9,3 +15,6 @@ library(enmtools)
 # PCA with varimax rotation and Kaiser criterion (eigenvalues greater than or equal to 1) when choosing factors to include in PCA
 
 ## assessing niche overlap
+nicheOverlap(rDip, rTetra, stat='D', mask=TRUE, checkNegatives=TRUE) # D statistic
+nicheOverlap(rDip, rTetra, stat='I', mask=TRUE, checkNegatives=TRUE) # I statistic
+nicheEquivalency()
